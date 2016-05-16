06e6db7dfcedf4d866145902cff2f6c31e43ec6c
Ricardo <riboher@gmail.com>
2016-05-16 18:17:01 +0200

    Added scripts for refills components

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  }); 
+
+  // var grid = $('.row').isotope({
+  //   		itemSelector: '.item',
+  //   	  	masonry: {
+  //   	  		percentPosition: true,
+  //   	    	columnWidth: '.item',
+  //   	    	// gutter: 20,
+  //   	    	fitWidth: true
+  //   	  	}
+  // });
+  // grid.imagesLoaded().progress(function() {
+  //   grid.isotope('layout');
+  // });
+
+
+  // $('.js-sticky-bar').sticky({topSpacing:20});
+  $('.item-box').mouseenter(function(){
+      $(this).find('.add-item').css('opacity',1);
+      $(this).find('.add-item').css('transform','scale(1)');
+    }).mouseleave(function(){
+      $(this).find('.add-item').css('opacity',0);
+      $(this).find('.add-item').css('transform','scale(0)');
+  });
+});
