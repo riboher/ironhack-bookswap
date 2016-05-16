a920c20ce3332010322d52bf0d46da24e1f30265
Ricardo <riboher@gmail.com>
2016-05-16 18:19:13 +0200

    Test for retrieve books according to the user login

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,12 +3,21 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   describe '#get_available_books' do
     it 'retrieves all books but the ones from the current user' do
-      user = create(:reader, id: 1)
+      current_user = create(:reader, id: 1)
       book1 = create(:book, reader_id: 2)
       book2 = create(:book, reader_id: 2)
       book3 = create(:book, reader_id: 2)
       book4 = create(:book, reader_id: 1, title: "Post Office")
-      expect(Book.get_available_books(user).length).to eq(3)
+      expect(Book.get_available_books(current_user).length).to eq(3)
+    end
+
+    it 'retrieves all books if no user is logged in' do
+      current_user = nil
+      book1 = create(:book, reader_id: 2)
+      book2 = create(:book, reader_id: 2)
+      book3 = create(:book, reader_id: 2)
+      book4 = create(:book, reader_id: 1, title: "Post Office")
+      expect(Book.get_available_books(current_user).length).to eq(4)
     end
   end
 end
