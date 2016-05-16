84230c12c7b718e1c0e7d265f5203a5aa6af5600
Ricardo <riboher@gmail.com>
2016-05-16 18:18:09 +0200

    Added method to retrieve books in index

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,10 @@ class Book < ActiveRecord::Base
   belongs_to :reader
 
   def self.get_available_books(current_reader)
-    all.where.not(reader_id: current_reader.id).where(is_available: true)
+    if current_reader != nil
+      all.where.not(reader_id: current_reader.id).where(is_available: true)
+    else
+      all
+    end
   end
 end
