80ef6c8983ed71ff9e3cee80f4eb9691dd51c268
Ricardo <riboher@gmail.com>
2016-05-17 19:50:39 +0200

    Added show layout and update action

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -2,4 +2,19 @@ class ReadersController < ApplicationController
   before_action :authenticate_reader!
   def show
   end
+
+  def update
+    respond_to do |format|
+      if current_reader.update(reader_params)
+        format.html { redirect_to reader_path , notice: "Your info was succesfully updated" }
+      else
+        format.html { redirect_to reader_path , notice: "Something went wrong. It wasn't possible to update your info"}
+      end
+    end
+  end
+
+  private
+  def reader_params
+    params.require(:reader).permit(:first_name, :last_name, :email, :address, :postcode, :city)
+  end
 end
