2870c1bb8b34cf267f6b7791573fd6520c6ae62f
Ricardo <riboher@gmail.com>
2016-05-17 12:21:22 +0200

    Modified author formatting helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,6 @@
 module BooksHelper
   def format_author(author)
     author_full_name = author.split(",")
-    formatted_name = author_full_name[1] + " " + author_full_name [0]
+    author_full_name[1] + " " + author_full_name [0] unless author_full_name.length == 1
   end
 end
