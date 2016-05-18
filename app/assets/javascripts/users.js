c43899a32fefba29992ef6c35f5df8eaae8e8f0e
Ricardo <riboher@gmail.com>
2016-05-18 10:33:39 +0200

    Implemented ajax call with button filters!

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,26 @@
-$(function(){
+$(document).on('page:change',function(){
 	$('.edit-button').on('click',function(event){
 		event.preventDefault();
 		var attribute = $(this).data('user');
 		$('.'+attribute).prop('disabled',false);
 	});
+
+	var filters = $('.js-book-filters').find('input[type="radio"]');
+	filters.on('click',function(){
+		var action = $(this).val();
+		var baseUrl = window.location.href;
+		$.ajax({
+			method: 'get',
+			url: baseUrl + "/" + action,
+			success: getBooks,
+			error: function(){
+				var msg= "We didn't find any books with that request. Please try later";
+				flashNotice('error',msg);
+			}
+		});
+	});
+
+	function getBooks(books){
+		console.log(books);
+	}
 });
