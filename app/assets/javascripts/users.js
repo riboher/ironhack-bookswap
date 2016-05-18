39afba75aa059b7bc5ff4ae7f4d9184041fc0ab9
Ricardo <riboher@gmail.com>
2016-05-18 11:00:05 +0200

    Added functions to parse the json and appended it to the user's panel

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -7,20 +7,46 @@ $(document).on('page:change',function(){
 
 	var filters = $('.js-book-filters').find('input[type="radio"]');
 	filters.on('click',function(){
+		$('.js-append-books').empty();
 		var action = $(this).val();
+		callBooks(action);
+	});
+
+
+	function callBooks(action){
 		var baseUrl = window.location.href;
 		$.ajax({
 			method: 'get',
 			url: baseUrl + "/" + action,
 			success: getBooks,
 			error: function(){
-				var msg= "We didn't find any books with that request. Please try later";
+				var msg= "We didn't find any books with that request. Please try again later";
 				flashNotice('error',msg);
 			}
 		});
-	});
+	}
 
 	function getBooks(books){
-		console.log(books);
+		books.forEach(function(book){
+			var book = parseBook(book.title,book.author,book.cover,book.price);
+			$('.js-append-books').append(book);
+		});
+	}
+
+	function parseBook(title,author,cover,price) {
+		return '<div class="item">'+
+					'<section class="item-box">'+
+						'<article class="item-box-actions">'+
+							'<a href="#" class="item-price">'+price+'<i class="fa fa-connectdevelop"></i></a>'+
+						'</article>'+
+						'<article class="item-thumbnail">'+
+							'<img src='+cover+'>'+
+						'</article>'+
+						'<article class="item-inner-elements">'+
+							'<h6 class="item-title">'+title+'</h6>'+
+							'<p class="item-author">'+author+'</p>'+
+						'</article>'+
+					'</section>'+
+				'</div>';
 	}
 });
