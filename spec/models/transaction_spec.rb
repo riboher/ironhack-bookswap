76cc859d1eb56fc916b8aa1b8a7974000ce7cecd
Ricardo <riboher@gmail.com>
2016-05-18 14:26:16 +0200

    Added tests for transaction methods

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#get books from buyer' do
+    it 'returns all the books for a specific buyer' do
+      buyer = create(:reader, id: 2)
+      create(:book, reader_id: 2)
+      create(:book, reader_id: 1)
+      create(:book, reader_id: 2)
+      Transaction.create(book_id: 1, buyer_id: 2, seller_id: 1)
+      Transaction.create(book_id: 2, buyer_id: 1, seller_id: 2)
+      Transaction.create(book_id: 3, buyer_id: 2, seller_id: 1)
+      expect(Transaction.get_buyer_books(buyer).length).to eq(2)
+    end
+  end
+
+  describe '#get books from seller' do
+    it 'returns all the books for a specific seller' do
+      seller = create(:reader, id: 2)
+      create(:book, reader_id: 2)
+      create(:book, reader_id: 1)
+      create(:book, reader_id: 2)
+      Transaction.create(book_id: 1, buyer_id: 1, seller_id: 2)
+      Transaction.create(book_id: 2, buyer_id: 2, seller_id: 1)
+      Transaction.create(book_id: 3, buyer_id: 2, seller_id: 1)
+      expect(Transaction.get_seller_books(seller).length).to eq(1)
+    end
+  end
 end
