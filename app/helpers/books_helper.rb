8435062bcb9e8ad796c762ca1533b3f0e45d2002
Ricardo <riboher@gmail.com>
2016-05-19 15:10:41 +0200

    Added helper methods for buttons

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -3,4 +3,20 @@ module BooksHelper
     author_full_name = author.split(",")
     author_full_name[1] + " " + author_full_name [0] unless author_full_name.length == 1
   end
+
+  def format_owner_name(first_name,last_name)
+    if last_name != nil
+      first_name + last_name.split("")[0]
+    else
+      first_name
+    end
+  end
+
+  def compare_credit(price,credit)
+    button = "enabled"
+    if price > credit
+      button = "disabled"
+    end
+    button
+  end
 end
