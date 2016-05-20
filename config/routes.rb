e43569fe23729569fc94fb6a040c7c8ccdc5bcb5
Ricardo <riboher@gmail.com>
2016-05-20 10:50:47 +0200

    Changed devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :readers, controllers: { registrations: "readers/registrations" }
+  devise_for :readers, path: '/'
   resources :readers
 
   root :to => 'books#index'
