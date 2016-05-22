f3380e1c2d5ac12aa6d95e98177dc507d0e16f50
Ricardo <riboher@gmail.com>
2016-05-22 12:18:01 +0200

    Fixed the joins query to be ordered by transactions.created_at attribute

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,11 +3,11 @@ class Transaction < ActiveRecord::Base
   belongs_to :seller, class_name: 'Reader'
 
   def self.get_buyer_books(reader)
-   Book.joins(:transactions).where("buyer_id = ?",reader.id).order(created_at: :desc)
+   Book.joins(:transactions).where("buyer_id = ?",reader.id).order('transactions.created_at desc')
   end
 
   def self.get_seller_books(reader)
-    Book.joins(:transactions).where("seller_id = ?",reader.id).order(created_at: :desc)
+    Book.joins(:transactions).where("seller_id = ?",reader.id).order('transactions.created_at desc')
   end
 
   def self.credit_update(book,buyer,seller)
