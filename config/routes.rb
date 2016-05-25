7f06e4fa48071d70533f0dfabe49dec558d28ff3
Ricardo <riboher@gmail.com>
2016-05-25 18:20:07 +0200

    Created proxy to avoid CORS

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :readers, path: "/"
   resources :readers
 
+  namespace :api do
+    namespace :v1 do
+      get '/books/search/:isbn' => "books#isbn_search"
+    end
+  end
+
   root :to => 'books#index'
 
   get  '/readers/:id/my_books'        => "readers#my_books", defaults: { format: :json }
