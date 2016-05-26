b8dfb9b24833364d37e32b05e890ffc86ef73459
Ricardo <riboher@gmail.com>
2016-05-26 12:16:22 +0200

    Added flash error when the crdit is not enough

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -11,9 +11,12 @@ class TransactionsController < ApplicationController
           session[:transaction_id] = transaction.id
           format.html { redirect_to reader_path(current_reader.id), notice: "" }
         else
-          format.html { redirect_to root_path, notice: "The purchase was not completed" }
+          format.html { redirect_to root_path, notice: "La compra no ha sido completada" }
         end
       end
+    else
+      flash[:error] = "No tienes crédito suficiente! Cambia más libros y obtendrás más puntos"
+      redirect_to root_path
     end
   end
 end
