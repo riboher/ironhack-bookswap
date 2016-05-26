b23f712903cbc607fdf77ca0101d9d494a2d9807
Ricardo <riboher@gmail.com>
2016-05-26 17:23:26 +0200

    Added confirmation email

--- app/mailers/reader_mailer.rb
+++ app/mailers/reader_mailer.rb
@@ -4,6 +4,13 @@ class ReaderMailer < ApplicationMailer
     @book = book
     @book_owner = @book.reader
     mail(to: @book_owner.email, subject: """Hola #{@book_owner.first_name}!
-      Alguien ha hecho Swap a tu libro #{book.title}""")
+      Alguien ha hecho Swap a tu libro #{@book.title}""")
+  end
+
+  def confirmation_swap_mail(book,current_reader)
+    @book = book
+    @reader = current_reader
+    mail(to: @reader.email, subject: """Hola #{@reader.first_name}!
+      Has hecho Swap a  #{@book.title}""")
   end
 end
